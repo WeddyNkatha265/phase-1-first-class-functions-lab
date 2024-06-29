fc7b432e6d2c41ce316fce07db09932857832536
Weddy Nkatha <weddynkatha265@gmail.com>
2024-06-29 14:18:32 +0300

    Updates first class functions

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+const drivers = ["Antonia", "Nuru", "Amari", "Mo"];
+const returnFirstTwoDrivers = function (drivers) {
+  return [drivers[0], drivers[1]];
+};
+returnFirstTwoDrivers(drivers);
+console.log(returnFirstTwoDrivers);
+//second
+const returnLastTwoDrivers = function (drivers) {
+  return [drivers[2], drivers[3]];
+};
+console.log(returnLastTwoDrivers);
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function (multiplier) {
+  const fareMultiplier = function (fare) {
+    return fare * multiplier;
+  };
+  return fareMultiplier;
+};
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, fn) {
+  return fn(drivers);
+}
+
+// Example usage:
+const firstTwo = selectDifferentDrivers(drivers, returnFirstTwoDrivers);
+const lastTwo = selectDifferentDrivers(drivers, returnLastTwoDrivers);
